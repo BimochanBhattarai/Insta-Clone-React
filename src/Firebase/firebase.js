9d3907046f467aed3c39db7fbac33cf8fabb7447
BimochanBhattarai <itsbimochan@gmail.com>
2025-01-18 23:07:17 +0545

    Firebase Update

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,10 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCSSvWHrBlrS9_FytnHcqf3FXLvE4IdwJw",
   authDomain: "insta-clone-react-9c02d.firebaseapp.com",
@@ -15,6 +13,9 @@ const firebaseConfig = {
   measurementId: "G-4GSW1SB7NR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, firestore, storage };
